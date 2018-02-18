cd8ca37546666b4739e191a03de93f9239e5a001
Kolja Lubitz <pinguin999@gmail.com>
2018-02-18 14:09:04 +0100

    test flaks server update mit CORS

--- car.py
+++ car.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask.ext.jsonpify import jsonify
 from iota_api import Car
+from flask_cors import CORS
 
 try:
     import configparser
@@ -14,25 +15,28 @@ except Exception:
     seed = os.environ['SEED']
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 car = Car(seed)
 
 
-class Identify(Resource):
-    def get(self):
-        address = str(car.getAddress(1)['addresses'][0])
-        return {'identity': address}
+# class Identify(Resource):
+@app.route("/identify")
+def Identify(self):
+    address = str(car.getAddress(1)['addresses'][0])
+    return {'identity': address}
 
 
-class Payment(Resource):
-    def get(self, adress, amount=0):
-        car.sendiota(adress, float(amount))
-        return {'payment': 'OK'}
+# class Payment(Resource):
+@app.route("/payments/<adress>/<amount>")
+def Payment(self, adress, amount=0):
+    car.sendiota(adress, float(amount))
+    return {'payment': 'OK'}
 
 
-api.add_resource(Identify, '/identify')  # Route_1
-api.add_resource(Payment, '/payments/<adress>/<amount>')  # Route_2
+# api.add_resource(Identify, '/identify')  # Route_1
+# api.add_resource(Payment, '/payments/<adress>/<amount>')  # Route_2
 
 
 if __name__ == '__main__':
